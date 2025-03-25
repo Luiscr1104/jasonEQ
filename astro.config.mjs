037a666705e5ef8112c9a8ffdf242654544f2293
Luis R Zunniga <luiscr1104@gmail.com>
2025-03-25 15:22:45 -0600

    Agrego form sin acciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/components/icons/index.ts
@@ -0,0 +1,16 @@
+import Quote from "./Quote.astro";
+
+export { default as PhoneCall } from "./PhoneCall.astro";
+export { default as Menu } from "./Menu.astro";
+export { default as Clock } from "./clock.astro";
+export { default as Percentage } from "./percentage.astro";
+export { default as HeartHandshake } from "./heart-handshake.astro";
+export { default as MapPin } from "./map-pin-bolt.astro";
+export { default as Seedling } from "./seedling.astro";
+export { default as Shield } from "./shield.astro";
+export { default as Trending } from "./trending-up.astro";
+export { default as Check } from "./user-check.astro";
+export { default as ArrowUpRight } from "./ArrowUpRight.astro";
+export { default as Star } from "./Star.astro";
+export { default as Quote } from "./Quote.astro";
+export { default as Mail } from "./Mail.astro";
