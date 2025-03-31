bcc13bcd7690254ccb19a756a8c172186a996e84
Luis R Zunniga <luiscr1104@gmail.com>
2025-03-31 11:11:51 -0600

    Agrego api dyanmics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node"; // 👈 Importar el adaptador
 
 export default defineConfig({
+  output: "server", // 👈 Importante para permitir SSR/API routes
+  adapter: node({ mode: "standalone" }), // 👈 Adaptador Node
+
   vite: {
     resolve: {
       alias: {
