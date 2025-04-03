ab838dc8ff743f45f0879f5b2bb6a92bdb1eb4a7
Luis R Zunniga <luiscr1104@gmail.com>
2025-04-03 14:43:30 -0600

    agrego sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
+import sitemap from "astro-sitemap";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  site: "https://www.terrenoslafortunacr.com",
+  integrations: [sitemap()],
   output: "server",
   adapter: vercel(),
   vite: {
