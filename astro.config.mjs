01ade109bfcdd9873be107610e11fbd1925c87de
Luis R Zunniga <luiscr1104@gmail.com>
2025-04-13 00:19:25 -0600

    Configuro alphgine con entrypoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
   site: "https://www.terrenoslafortunacr.com",
-  integrations: [
-    sitemap(),
-    markdoc(),
-    alpinejs({ entrypoint: "/src/entrypoint" }),
-  ],
+  integrations: [sitemap(), markdoc(), alpinejs({ entrypoint: "/entrypoint" })],
   output: "server",
   adapter: vercel(),
   vite: {
